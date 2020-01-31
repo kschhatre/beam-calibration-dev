ede0b81e1a96e5f0542a0bab7c223df315ecc7bd
Kiran Chhatre <kiranchhatre3@gmail.com>
2020-01-30 19:57:42 -0800

    code modularize and test

--- TUNE_integrate/TUNE_test/worker_n_optimizer/imported_scripts/configselector.py
+++ TUNE_integrate/TUNE_test/worker_n_optimizer/imported_scripts/configselector.py
@@ -2,6 +2,8 @@ import glob
 import os
 from sys import argv
 
+simulation = 'beamville' 
+# 'beamville' or 'urbansim-1k' or 'urbansim-10k'
 
 def configselector():
     workers = 4
@@ -13,12 +15,16 @@ def configselector():
 
 
     def returnConfFile(count):
-        Dir = '/home/berkeleylab/Repository/beam/test/input/beamville/'
-        conf_files = [file for file in glob.glob(os.path.join(Dir, 'beam_*.conf'))] 
+        
+        if simulation == 'beamville':
+            Dir = '/home/berkeleylab/Repository/beam/test/input/'+simulation+'/'+simulation[:4] 
+        else:
+            Dir = '/home/berkeleylab/Repository/beam/test/input/sf-light/'+simulation
+
+        conf_files = [file for file in glob.glob(os.path.join(Dir+'_*.conf'))] 
         name_dir = [] 
         for name in conf_files:
-            a = name
-            name_dir.append(a)
+            name_dir.append(name)
         current_conf_file = name_dir[count]
         return current_conf_file
         
@@ -29,6 +35,3 @@ def configselector():
         f.write(str(count+1))
     
     return address
-
-#workers = argv[1]
-#configselector()
--- TUNE_integrate/TUNE_test/worker_n_optimizer/imported_scripts/runme.sh
+++ TUNE_integrate/TUNE_test/worker_n_optimizer/imported_scripts/runme.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-file="/home/berkeleylab/Repository/beam/test/input/beamville/instanceconfpath.txt"
+file="/home/berkeleylab/Repository/beam/instanceconfpath.txt"
 
 conf_file=$(cat "$file")
-
 
 gradle :run -PappArgs="['--config', '$conf_file']"
 
